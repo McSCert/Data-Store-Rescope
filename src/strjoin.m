e9c89ef0b655e8374b8212707d6067d2944afb19
Monika Jaskolka <bialy.monika@gmail.com>
2016-06-01 21:27:53 +0000

    Deleted comments with the old version of the implementation. Added link to Matlab Central
    git-svn-id: https://www.mcscert.ca/leap/svn/trunk/sw/tools/Simulink/forFCA/McMaster_Tools/DataStoreRescope@6758 9a9db257-bd8a-4a57-83e7-af9ab8ab1356

--- src/strjoin.m
+++ src/strjoin.m
@@ -1,30 +1,6 @@
 function output = strjoin(input, separator)
-%STRJOIN Concatenate an array into a single string.
-%
-%     S = strjoin(C)
-%     S = strjoin(C, separator)
-%
-% Description
-%
-% S = strjoin(C) takes an array C and returns a string S which concatenates
-% array elements with comma. C can be a cell array of strings, a character
-% array, a numeric array, or a logical array. If C is a matrix, it is first
-% flattened to get an array and concateneted. S = strjoin(C, separator) also
-% specifies separator for string concatenation. The default separator is comma.
-%
-% Examples
-%
-%     >> str = strjoin({'this','is','a','cell','array'})
-%     str =
-%     this,is,a,cell,array
-%
-%     >> str = strjoin([1,2,2],'_')
-%     str =
-%     1_2_2
-%
-%     >> str = strjoin({1,2,2,'string'},'\t')
-%     str =
-%     1 2 2 string
+
+% http://www.mathworks.com/matlabcentral/fileexchange/31862-strjoin
 %
 % Copyright (c) 2011, Kota Yamaguchi
 % All rights reserved.
