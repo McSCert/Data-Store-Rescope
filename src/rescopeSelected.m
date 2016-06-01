91819ef625f7fa777f40451e3c7f7017aa93c792
Bennett Mackenzie <mackeb@mcmaster.ca>
2016-06-01 15:13:48 +0000

    added assertions to verify that arguments for rescopeSelected.m are correct
    git-svn-id: https://www.mcscert.ca/leap/svn/trunk/sw/tools/Simulink/forFCA/McMaster_Tools/DataStoreRescope@6737 9a9db257-bd8a-4a57-83e7-af9ab8ab1356

--- src/rescopeSelected.m
+++ src/rescopeSelected.m
@@ -13,18 +13,50 @@ function rescopeSelected(model, dataStores)
 %   rescopeSelected(bdroot, gcbs)    % rescope the selected Data Store Memory
 %                                    % blocks in the current Simulink system
 
+    % Perform a check to see if the model is open and unlocked
+    try
+        assert(strcmp(get_param(bdroot(model), 'Lock'), 'off'))
+    catch E
+        if strcmp(E.identifier, 'MATLAB:assert:failed') || ... 
+                strcmp(E.identifier, 'MATLAB:assertion:failed')
+            disp(['Error using ' mfilename ':' char(10) ...
+                ' File is locked.'])
+            return
+        else
+            disp(['Error using ' mfilename ':' char(10) ...
+                ' Invalid model name argument M.' char(10)])
+            help(mfilename)
+            return
+        end
+    end
+
     toRescope = {};
     for i = 1:length(dataStores)
+        
         % Check if block in list dataStores is a Data Store block
+        blockType=get_param(dataStores{i}, 'BlockType');
         try
-            dataStoreName = get_param(dataStores{i}, 'DataStoreName');
+            assert(strcmp(blockType, 'DataStoreRead') || ...
+                strcmp(blockType, 'DataStoreWrite') || ...
+                strcmp(blockType, 'DataStoreMemory'));
         catch E
-            disp(['Error using ' mfilename ':' char(10) ...
-                ' Selected block ' getfullname(dataStores{i}) ' is not a Data Store.'])
+            if strcmp(E.identifier, 'MATLAB:assert:failed') || ...
+                    strcmp(E.identifier, 'MATLAB:assertion:failed')
+                disp(['Error using ' mfilename ':' char(10) ...
+                    getfullname(dataStores{i}) ' is not a data store block.'])
+                continue
+            else
+                disp(['Error using ' mfilename ':' char(10) ...
+                ' Invalid data store list argument D.' char(10)])
+                help(mfilename)
+                return
+            end
         end
         
+        dataStoreName = get_param(dataStores{i}, 'DataStoreName');
+
         % Check if selected data store block is the DataStoreMemory block
-        if ~strcmp(get_param(dataStores{i}, 'BlockType'), 'DataStoreMemory')
+        if ~strcmp(blockType, 'DataStoreMemory')
             % If not, find corresponding DataStoreMemory block before adding
             temp = find_system(model, 'BlockType', 'DataStoreMemory', 'DataStoreName', dataStoreName);
             toRescope{end + 1} = temp{1};
