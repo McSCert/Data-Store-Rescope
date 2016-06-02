c9d65e162763308e495692ce8c6146e93b9fd63e
Bennett Mackenzie <mackeb@mcmaster.ca>
2016-06-02 20:42:01 +0000

    git-svn-id: https://www.mcscert.ca/leap/svn/trunk/sw/tools/Simulink/forFCA/McMaster_Tools/DataStoreRescope@6781 9a9db257-bd8a-4a57-83e7-af9ab8ab1356

--- src/dataStoreRescope.m
+++ src/dataStoreRescope.m
@@ -249,7 +249,7 @@ function dataStoreRescope(model, dontmove)
                     flag = false;
                 catch E
                     if strcmp(E.identifier, 'Simulink:Commands:AddBlockCantAdd')
-                        Name = [oldName '_' num2str(n)];
+                        Name = [oldName ' ' num2str(n)];
                         n = n + 1;
                     end
                 end
